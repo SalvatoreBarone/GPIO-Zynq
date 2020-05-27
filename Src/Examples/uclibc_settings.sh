fce4b8633b64cd2d85d0965b34968f004b839571
Salvatore Barone <salvator.barone@gmail.com>
2020-05-27 09:05:52 +0200

    Buildroot cross compilation

---
+++ Src/Examples/uclibc_settings.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+export CC=arm-buildroot-linux-uclibcgnueabihf-gcc
+export CXX=arm-buildroot-linux-uclibcgnueabihf-g++
+export LD=arm-buildroot-linux-uclibcgnueabihf-ld
+export AR=arm-buildroot-linux-uclibcgnueabihf-ar
+export OBJCOPY=arm-buildroot-linux-uclibcgnueabihf-objcopy
+export SIZE=arm-buildroot-linux-uclibcgnueabihf-size
+export PATH=/home/ssaa/GPIO_SE/ZyboLinux/buildroot-2020.02.2/output/host/bin:$PATH
